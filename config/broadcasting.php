787ce2e7e5b7f04cc9449a7061d6f53aa55ddb22
Abu Saleem <abusaleem>
2018-12-08 20:27:02 +0100

    pusher was  install and configured with client_message to false

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -29,7 +29,7 @@ return [
     */
 
     'connections' => [
-
+        
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
@@ -38,8 +38,12 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http'
             ],
         ],
+
 
         'redis' => [
             'driver' => 'redis',
