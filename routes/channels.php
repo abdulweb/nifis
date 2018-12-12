3020510834b4e489da7e7b1a93acafe3f3558815
Abu Saleem <abusaleem>
2018-12-12 20:58:01 +0100

    set route and views

--- routes/channels.php
+++ routes/channels.php
@@ -18,3 +18,8 @@ Broadcast::channel('App.User.{id}', function ($user, $id) {
 Broadcast::channel('chat', function ($user){
 	return $user;
 });
+
+Broadcast::channel('post.{id}', function ($user, $id) {
+    //return $user->id == \App\Post::find($id)->user_id;
+    return true;
+});
