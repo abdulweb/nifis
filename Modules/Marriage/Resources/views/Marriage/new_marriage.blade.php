47ba6b4ea449137e6d7bdab24e49ef4c22a32a1e
Abu Saleem <abusaleem>
2018-12-31 17:31:36 +0100

    added placeholder to marriage registration form

--- Modules/Marriage/Resources/views/Marriage/new_marriage.blade.php
+++ Modules/Marriage/Resources/views/Marriage/new_marriage.blade.php
@@ -12,139 +12,144 @@
 				<h3>Husband Info</h3>
 				<section>
 					<div class="form-group clearfix">
-						<label class="col-lg-3 control-label " for="userName1">Husband First Name</label>
+						<label class="col-lg-3 control-label " for="husband_last_name">Husband First Name</label>
 						<div class="col-lg-9">
-							<input palceholder="Husband First Name" class="form-control required" id="userName1" name="userName" type="text">
+							<input placeholder="Husband First Name" class="form-control required" id="userName1" name="husband_last_name" type="text">
 						</div>
 					</div>
 					<div class="form-group clearfix">
-						<label class="col-lg-3 control-label " for="password1">Husband Last Name</label>
+						<label class="col-lg-3 control-label " for="husband_last_name">Husband Last Name</label>
 						<div class="col-lg-9">
-							<input id="password1" name="password" type="text" class="required form-control">
+							<input placeholder="Husband Last Name"  id="husband_last_name" name="husband_last_name" type="text" class="required form-control">
 						</div>
 					</div>
 				</section>
 				<h3>Wife Info</h3>
 				<section>
 					<div class="form-group clearfix">
-						<label class="col-lg-3 control-label " for="userName1">Wife First Name</label>
+						<label class="col-lg-3 control-label " for="wife_first_name">Wife First Name</label>
 						<div class="col-lg-9">
-							<input class="form-control required" id="userName1" name="userName" type="text">
+							<input placeholder="Wife First Name" class="form-control required" id="wife_first_name" name="wife_first_name" type="text">
 						</div>
 					</div>
 					<div class="form-group clearfix">
-						<label class="col-lg-3 control-label " for="userName1">Wife last Name</label>
+						<label class="col-lg-3 control-label " for="wife_last_name">Wife last Name</label>
 						<div class="col-lg-9">
-							<input class="form-control required" id="userName1" name="userName" type="text">
+							<input placeholder="Wife Last Name"  class="form-control required" id="wife_last_name" name="wife_last_name" type="text">
 						</div>
 					</div>
 					<div class="form-group clearfix">
 						<label class="col-lg-3 control-label " for="userName1">Wife Status</label>
 						<div class="col-lg-9">
-							<input class="form-control required" id="userName1" name="userName" type="text">
+							<select name="w_status">
+								<option>Wife Status</option>
+								<option value="1">First Wife</option>
+								<option value="2">Second Wife</option>
+								<option value="3">Third Wife</option>
+								<option value="4">Forth Wife</option>
+							</select>
 						</div>
 					</div>
 				</section>
 				<h3>Marriage Info</h3>
 				<section>
 					<div class="form-group clearfix">
-						<label class="col-lg-3 control-label " for="userName1">Date Of Marriage</label>
+						<label class="col-lg-3 control-label " for="mdate">Date Of Marriage</label>
 						<div class="col-lg-9">
-							<input class="form-control required" id="userName1" name="userName" type="text">
+							<input class="form-control required" id="mdate" name="mdate" type="date">
 						</div>
 					</div>
 				</section>
 				<h3>Address Info</h3>
 				<section>
 					<div class="form-group clearfix">
-						<label class="col-lg-3 control-label " for="userName1">Country</label>
+						<label class="col-lg-3 control-label " for="country">Country</label>
 						<div class="col-lg-9">
-							<input class="form-control required" id="userName1" name="userName" type="text">
+							<input placeholder="Country" class="form-control required" id="country" name="country" type="text">
 						</div>
 					</div>
 					<div class="form-group clearfix">
-						<label class="col-lg-3 control-label " for="userName1">State</label>
+						<label class="col-lg-3 control-label " for="state">State</label>
 						<div class="col-lg-9">
-							<input class="form-control required" id="userName1" name="userName" type="text">
+							<input  placeholder="State"  class="form-control required" id="state" name="state" type="text">
 						</div>
 					</div>
 					<div class="form-group clearfix">
-						<label class="col-lg-3 control-label " for="userName1">Local Government</label>
+						<label class="col-lg-3 control-label " for="lga">Local Government</label>
 						<div class="col-lg-9">
-							<input class="form-control required" id="userName1" name="userName" type="text">
+							<input  placeholder="Local Government" class="form-control required" id="lga" name="lga" type="text">
 						</div>
 					</div>
                     <div class="form-group clearfix">
-						<label class="col-lg-3 control-label " for="userName1">Town/Village</label>
+						<label class="col-lg-3 control-label " for="town">Town/Village</label>
 						<div class="col-lg-9">
-							<input class="form-control required" id="userName1" name="userName" type="text">
+							<input  placeholder="Town/Village" class="form-control required" id="town" name="town" type="text">
 						</div>
 					</div>
 					<div class="form-group clearfix">
-						<label class="col-lg-3 control-label " for="userName1">Area</label>
+						<label class="col-lg-3 control-label " for="area">Area</label>
 						<div class="col-lg-9">
-							<input class="form-control required" id="userName1" name="userName" type="text">
+							<input  placeholder="Living Area"  class="form-control required" id="area" name="area" type="text">
 						</div>
 					</div>
 					<div class="form-group clearfix">
-						<label class="col-lg-3 control-label " for="userName1">House No</label>
+						<label class="col-lg-3 control-label " for="house_no">House No</label>
 						<div class="col-lg-9">
-							<input class="form-control required" id="userName1" name="userName" type="text">
+							<input  placeholder="House No" class="form-control required" id="house_no" name="house_no" type="text">
 						</div>
 					</div>
 					<div class="form-group clearfix">
-						<label class="col-lg-3 control-label " for="userName1">Description</label>
+						<label class="col-lg-3 control-label " for="house_desc">Description</label>
 						<div class="col-lg-9">
-							<input class="form-control required" id="userName1" name="userName" type="text">
+							<input  placeholder="House Description" class="form-control required" id="house_desc" name="house_desc" type="text">
 						</div>
 					</div>
 				</section>
 				<h3>Wife Family Info</h3>
 				<section>
 					<div class="widget widget-tabs widget-tabs-gray border-bottom-none">
-                                    <!-- Widget heading -->
-                                    <div class="widget-head">
-                                        <strong class="lead">Did the wife family account created ? </strong>
-                                        <button  class="active btn btn-primary"  href="#yes" data-toggle="tab">Yes</button>
-                                        <button class="btn btn-warning" value="" href="#no" data-toggle="tab">No</button>
-                                        <button class="btn btn-infor" value="" href="#why" data-toggle="tab">Why ?</button>
-                                    </div>
-                                    <!-- // Widget heading END -->
-                                    <div class="widget-body">
-                                        <div class="tab-content">
-                                            <!-- Tab content -->
-                                            <div class="tab-pane active" id="no">
-                                                <strong>We assume that you this wife's parent doesn't have family account if they have click <a  href="#yes" data-toggle="tab"><i></i>yes they have</a></li></strong>
-                                                <div class="form-group clearfix">
-                                                    <label class="col-lg-3 control-label " for="name">Wife Date Date Of Birth</label>
-                                                    <div class="col-lg-9">
-                                                        <input placeholder="Root First Name" class="form-control" value="{{ old('name') }}" id="name" name="name" type="text">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="tab-pane" id="why">
-                                                <strong>We want to establish connection between the wife family and husband family enable us to easely pass information from one family to another like invetation.  
-                                                </strong>
-                                            </div>
-                                            <div class="tab-pane" id="yes">
-                                                <div class="form-group clearfix">
-                                                    <label class="col-lg-3 control-label " for="name">Family Title</label>
-                                                    <div class="col-lg-9">
-                                                        <input placeholder="Root First Name" class="form-control" value="{{ old('name') }}" id="name" name="name" type="text">
-                                                    </div>
-                                                </div>
-
-                                                <div class="form-group clearfix">
-                                                    <label class="col-lg-3 control-label " for="sname">Wife E-mail address</label>
-                                                    <div class="col-lg-9">
-                                                        <input placeholder="Root Last Name" class="form-control" value="{{ old('sname') }}" id="sname" name="sname" type="text">
-                                                    </div>
-                                                </div>
-
-                                            </div>
-                                        </div>
+                    <!-- Widget heading -->
+                    <div class="widget-head">
+                        <strong class="lead">Did the wife family account created ? </strong>
+                        <button  class="active btn btn-primary"  href="#yes" data-toggle="tab">Yes</button>
+                        <button class="btn btn-warning" value="" href="#no" data-toggle="tab">No</button>
+                        <button class="btn btn-infor" value="" href="#why" data-toggle="tab">Why ?</button>
+                    </div>
+                    <!-- // Widget heading END -->
+                    <div class="widget-body">
+                        <div class="tab-content">
+                            <!-- Tab content -->
+                            <div class="tab-pane active" id="no">
+                                <strong>We assume that you this wife's parent doesn't have family account if they have click <a  href="#yes" data-toggle="tab"><i></i>yes they have</a></li></strong>
+                                <div class="form-group clearfix">
+                                    <label class="col-lg-3 control-label " for="name">Wife Date Date Of Birth</label>
+                                    <div class="col-lg-9">
+                                        <input placeholder="Root First Name" class="form-control" value="{{ old('name') }}" id="name" name="name" type="text">
                                     </div>
                                 </div>
+                            </div>
+                            <div class="tab-pane" id="why">
+                                <strong>We want to establish connection between the wife family and husband family enable us to easely pass information from one family to another like invetation.  
+                                </strong>
+                            </div>
+                            <div class="tab-pane" id="yes">
+                                <div class="form-group clearfix">
+                                    <label class="col-lg-3 control-label " for="wfamily">Family Title</label>
+                                    <div class="col-lg-9">
+                                        <input placeholder="Wife Family Title" class="form-control" value="{{ old('wfamily') }}" id="name" name="wfamily" type="text">
+                                    </div>
+                                </div>
+
+                                <div class="form-group clearfix">
+                                    <label class="col-lg-3 control-label " for="sname">Wife E-mail address</label>
+                                    <div class="col-lg-9">
+                                        <input placeholder="Wife E-mail Address" class="form-control" value="{{ old('wemail') }}" id="wemail" name="wemail" type="email">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 				</section>
 				<h3>Be Notify</h3>
 				<section>
