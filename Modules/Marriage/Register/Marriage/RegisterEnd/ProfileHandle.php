f0bcc03ba7725632a171d04ae4c4445654902bbe
Abu Saleem <abusaleem>
2019-01-01 11:16:22 +0100

    profile handle class was created

--- Modules/Marriage/Register/Marriage/RegisterEnd/ProfileHandle.php
+++ Modules/Marriage/Register/Marriage/RegisterEnd/ProfileHandle.php
@@ -1,1 +1,40 @@
-ProfileHandle.php
+<?php
+
+class ProfileHandle
+{
+    public $data;
+    
+    public $husbandProfile;
+    
+    public $wifeProfile;
+
+	public function __construct(User $user,$data)
+	{
+        $this->data = $data;
+        $this->husbandProfile = $user->profile()
+	}
+
+	public function handleWifeProfile()
+	{
+		if(empty($this->data['wfamily'])){
+            $user= User::create(['first_name'=>$this->data['wife_first_name'],'last_name'=>$this->data['wife_last_name']]);
+            $this->wifeProfile = $user->profile()->create(['gender_id'=>2,'marital_status_id'=>2]);
+		}else{
+            $user = User::where('email',$this->data['wemail']);
+            $this->wifeProfile = $user->profile();
+		}
+	}
+
+	public function updateHusbandProfile()
+	{
+		if($this->husbandProfile->marital_status_id != 2){
+			$this->husbandProfile->update(['marital_status_id'=>2])->save();
+		}
+	}
+
+	public function handle()
+	{
+        $this->updateHusbandProfile();
+        $this handleWifeProfile()
+	}
+}
