c98d84886fe65e1e7d73ec7813770ddb1f1d342b
Abu Saleem <abusaleem>
2019-01-03 00:42:44 +0100

    set error message for any user attempt to create multiple family

--- Modules/Family/Http/Middleware/HasFamilyMiddleware.php
+++ Modules/Family/Http/Middleware/HasFamilyMiddleware.php
@@ -17,6 +17,7 @@ class HasFamilyMiddleware
     public function handle(Request $request, Closure $next)
     {
         if(Auth()->User()->profile()){
+            session()->flash('error',['Sorry you either register your family or been registered as a member of other family in that case you can not create family account rather you register your marriage which can automatically create family for you under your registered family.']);
             return redirect('/home');
         }
         return $next($request);
--- Modules/Family/Http/Middleware/IsUsedMiddleware.php
+++ Modules/Family/Http/Middleware/IsUsedMiddleware.php
@@ -17,6 +17,7 @@ class IsUsedMiddleware
     public function handle(Request $request, Closure $next)
     {
         if(Auth()->User()->family()){
+            session()->flash('error',['Sorry you sorry you have register a family account with this you cannot register another one it due to some security reason for more reason contact us at nifis@nifis.net']);
             return redirect('/home');
         }
         return $next($request);
