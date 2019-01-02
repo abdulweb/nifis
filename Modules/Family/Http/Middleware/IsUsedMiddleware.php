dce9230fceb7e66c5049b6739134304d70df69af
Abu Saleem <abusaleem>
2019-01-03 00:16:21 +0100

    create middle ware to restrict a user from c registring multiple family with tha sameme account

---
+++ Modules/Family/Http/Middleware/IsUsedMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Family\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsUsedMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request);
+    }
+}
