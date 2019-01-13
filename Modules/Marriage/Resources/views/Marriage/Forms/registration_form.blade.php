7b982bbb263d34a3b115eecfc9a64a527484c6c2
Abu Saleem <abusaleem>
2019-01-13 23:57:49 +0100

    update husband information on the form

--- Modules/Marriage/Resources/views/Marriage/Forms/registration_form.blade.php
+++ Modules/Marriage/Resources/views/Marriage/Forms/registration_form.blade.php
@@ -44,18 +44,65 @@
 			</div>
 		</div>
 		@else
-        <div class="form-group clearfix">
-			<label class="col-lg-4 control-label " for="husband_first_name">Husband First Name</label>
-			<div class="col-lg-8">
-				<input placeholder="Husband First Name" class="form-control required" id="userName1" name="husband_first_name" type="text" value="{{old('husband_first_name')}}">
-			</div>
-		</div>
-		<div class="form-group clearfix">
-			<label class="col-lg-4 control-label " for="husband_last_name">Husband Last Name</label>
-			<div class="col-lg-8">
-				<input value="{{old('husband_last_name')}}" placeholder="Husband Last Name"  id="husband_last_name" name="husband_last_name" type="text" class="required form-control">
-			</div>
-		</div>
+		<section>
+		<div class="widget widget-tabs widget-tabs-gray border-bottom-none">
+        <!-- Widget heading -->
+        <div class="widget-head">
+            <strong class="lead">Did the Husband has family account created ? </strong>
+            <button  class="active btn btn-primary"  href="#yes" data-toggle="tab">Yes</button>
+            <button class="btn btn-warning" value="" href="#no" data-toggle="tab">No</button>
+            <button class="btn btn-infor" value="" href="#why" data-toggle="tab">Why ?</button>
+        </div>
+        <!-- // Widget heading END -->
+        <div class="widget-body">
+            <div class="tab-content">
+                <!-- Tab content -->
+                <div class="tab-pane active" id="default">
+                   <strong>Please answer question above !</strong>
+                </div>
+                
+                <div class="tab-pane" id="why">
+                    <strong>You need to have a family account in order to register any marriage and establish connection between the wife family and husband family to enable us to easely pass information from one family to another like invetation.  
+                    </strong>
+                </div>
+
+                <div class="tab-pane" id="no">
+                    <strong>We assume that this husband doesn't have family account by filling this form we will automatically create one for him but if he has click here <a  href="#yes" data-toggle="tab"><i></i>yes he has</a></li></strong>
+                    <div class="form-group clearfix">
+						<label class="col-lg-4 control-label " for="husband_first_name">Husband First Name</label>
+						<div class="col-lg-8">
+							<input placeholder="Husband First Name" class="form-control required" id="userName1" name="husband_first_name" type="text" value="{{old('husband_first_name')}}">
+						</div>
+					</div>
+					<div class="form-group clearfix">
+						<label class="col-lg-4 control-label " for="husband_last_name">Husband Last Name</label>
+						<div class="col-lg-8">
+							<input value="{{old('husband_last_name')}}" placeholder="Husband Last Name"  id="husband_last_name" name="husband_last_name" type="text" class="required form-control">
+						</div>
+					</div>
+					<div class="form-group clearfix">
+						<label class="col-lg-4 control-label " for="husband_last_name">Husband Email</label>
+						<div class="col-lg-8">
+							<input value="{{old('husband_last_name')}}" placeholder="Husband Last Name"  id="husband_last_name" name="husband_last_name" type="text" class="required form-control">
+						</div>
+					</div>
+                </div>
+
+                <!--wife family tab-->
+                <div class="tab-pane" id="yes">
+                     <div class="form-group clearfix">
+                        <label class="col-lg-4 control-label " for="name">Husband Email</label>
+                        <div class="col-lg-8">
+                            <input placeholder="Husband Family E-mail Address" class="form-control" value="{{ old('wife_email') }}" id="name" name="husband_email" type="email">
+                        </div>
+                    </div>
+                </div>
+                <!-- end wife family tab-->
+            </div>
+        </div>
+    </div>
+	</section>
+     
 		@endif
 	</section>
 	<h3>Wife Info</h3>
