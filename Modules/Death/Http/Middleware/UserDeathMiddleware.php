44c96cb63b3c5909860f2f38709c40c0ce4e922f
Abu Saleem <abusaleem>
2019-01-20 22:58:55 +0100

    create user death middleware

---
+++ Modules/Death/Http/Middleware/UserDeathMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Death\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class UserDeathMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request);
+    }
+}
