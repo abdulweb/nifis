fcae13991eeeb4c4735ef8aff56d912f858be5f3
Abu Saleem <abusaleem>
2019-01-21 15:07:04 +0100

    set event for broadcasting

--- Modules/Divorce/Events/NewDivorceEvent.php
+++ Modules/Divorce/Events/NewDivorceEvent.php
@@ -2,9 +2,16 @@
 
 namespace Modules\Divorce\Events;
 
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class NewDivorceEvent
+class NewDivorceEvent ShouldBroadcastNow
 {
     use SerializesModels;
 
@@ -13,7 +20,7 @@ class NewDivorceEvent
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($data)
     {
         //
     }
