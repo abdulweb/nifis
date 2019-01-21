25c8ff0f05eb6915dad0ca5970b0b54f0498a666
Abu Saleem <abusaleem>
2019-01-21 04:17:34 +0100

    remove the duplicate module

--- Modules/Divorce/Database/Migrations/2018_11_16_190526_create_divorces_table.php
+++ Modules/Divorce/Database/Migrations/2018_11_16_190526_create_divorces_table.php
@@ -15,9 +15,8 @@ class CreateDivorcesTable extends Migration
     {
         Schema::create('divorces', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('marriage_id')->unsigned()->nullable()->foreign()->refernces('id')->on('marriages')->delete('restrict')->update('cascade');
             $table->integer('counter');
-            $table->integer('married_id')->unsigned()->default(0)->nullable();
-            $table->integer('date')->nullable();
             $table->integer('is_active')->unsigned()->default(1)->nullable();
             $table->timestamps();
         });
