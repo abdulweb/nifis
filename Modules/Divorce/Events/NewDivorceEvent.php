8b73d0a59c6c0bb25553169b8caff5fe832941a6
Abu Saleem <abusaleem>
2019-01-21 11:13:09 +0100

    create newDivorceEvent

---
+++ Modules/Divorce/Events/NewDivorceEvent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\Divorce\Events;
+
+use Illuminate\Queue\SerializesModels;
+
+class NewDivorceEvent
+{
+    use SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return [];
+    }
+}
