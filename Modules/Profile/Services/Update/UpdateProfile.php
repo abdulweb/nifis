5b7a603f15a15a5e6aa9de5efe9fac6ed995c831
Abu Saleem <abusaleem>
2019-01-22 23:12:55 +0100

    update profile class created

--- Modules/Profile/Services/Update/UpdateProfile.php
+++ Modules/Profile/Services/Update/UpdateProfile.php
@@ -1,5 +1,11 @@
 <?php
 
+namespace Modules\Profile\Services\Update;
+
+use Modules\Address\Services\LivingAddress;
+
+use Modules\Address\Services\WorkAddress;
+
 /**
 * this class will recieved the user information and update his profile
 */
@@ -10,7 +16,60 @@ class UpdateProfile
 	function __construct($data)
 	{
 		$this->data = $data;
+		$this->user = $data['user'];
+		$this->update();
 	}
 
-	use WorkAddress,HouseAddress;
+	protected function update()
+	{
+		switch ($this->data['submit']) {
+            case 'new_contact':
+                # code...
+                break;
+            case 'change_profile_image':
+                # code...
+                break;
+            
+            case 'new_certificate':
+                # code...
+                break;
+            
+            case 'profile_image':
+                # code...
+                break;
+            
+            case 'new_experience':
+                # code...
+                break;
+            
+            case 'new_skill':
+                # code...
+                break;
+            
+            case 'work_history':
+                # code...
+                break;
+            case 'new_business':
+                # code...
+                break;
+            case 'business_address':
+                use WorkAddress;
+                $this->user->profile->work->update(['address_id'=>$this->address()]);
+                session()->flash('message','The profile business address updated');
+                break;
+            case 'home_address':
+                use LivingAddress;
+                $this->user->profile->leave->update(['address_id'=>$this->address()]);
+                session()->flash('message','The profile home address updated');
+                break;
+            
+            case 'new_biography':
+               # code...
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+	}
 }
