6ad3446186d037f6602ef2c282df463ce1295da6
Abu Saleem <abusaleem>
2019-01-23 10:50:45 +0100

    display user home address on profile details

--- Modules/Profile/Resources/views/Forms/profile_setting.blade.php
+++ Modules/Profile/Resources/views/Forms/profile_setting.blade.php
@@ -103,7 +103,7 @@
                             <div class="form-group">
                                 <label class="col-md-3 control-label">Gender</label>
                                 <div class="col-md-9">
-                                    <input type="text" name="gender" value="{{'gender'}}" class="form-control">
+                                    <input type="text" name="gender" value="{{$user->profile->gender->name}}" class="form-control">
                                 </div>
                             </div>
                             <!-- // Group END -->
@@ -112,7 +112,7 @@
                             <div class="form-group">
                                 <label class="col-md-3 control-label">Age</label>
                                 <div class="col-md-9">
-                                    <input type="text" value="{{'age'}}" class="form-control" />
+                                    <input type="text" value="{{floor($user->profile->date/31622400)}}" class="form-control" />
                                 </div>
                             </div>
                             <!-- // Group END -->
@@ -136,7 +136,7 @@
                                 <label class="col-md-3 control-label">Country</label>
                                 <div class="col-md-9">
                                     <div class="input-group">
-                                        <input type="text" value="{{'country'}}" class="form-control" />
+                                        <input type="text" value="{{$user->profile->leave != null ? $user->profile->leave->address->house->area->town->lga->state->country->name : ''}}" class="form-control" />
                                         <span class="input-group-addon" data-toggle="tooltip" data-container="body" data-placement="top" data-original-title="First name is mandatory"><i class="fa fa-question-circle"></i></span>
                                     </div>
                                 </div>
@@ -148,7 +148,7 @@
                                 <label class="col-md-3 control-label">State</label>
                                 <div class="col-md-9">
                                     <div class="input-group">
-                                        <input type="text" value="{{'state'}}" class="form-control" />
+                                        <input type="text" value="{{$user->profile->leave != null ? $user->profile->leave->address->house->area->town->lga->state->name : ''}}" class="form-control" />
                                         <span class="input-group-addon" data-toggle="tooltip" data-container="body" data-placement="top" data-original-title="Last name is mandatory"><i class="fa fa-question-circle"></i></span>
                                     </div>
                                 </div>
@@ -160,7 +160,7 @@
                                 <label class="col-md-3 control-label">Local Govt</label>
                                 <div class="col-md-9">
                                     <div class="input-group">
-                                        <input type="text" id="datepicker1" class="form-control" value="{{'local government'}}" />
+                                        <input type="text" id="datepicker1" class="form-control" value="{{$user->profile->leave != null ? $user->profile->leave->address->house->area->town->lga->name : ''}}" />
                                         <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
                                     </div>
                                 </div>
@@ -177,7 +177,7 @@
                             <div class="form-group">
                                 <label class="col-md-3 control-label">Town</label>
                                 <div class="col-md-9">
-                                    <input type="text" name="gender" value="{{'town'}}" class="form-control">
+                                    <input type="text" name="gender" value="{{$user->profile->leave != null ? $user->profile->leave->address->house->area->town->name : ''}}" class="form-control">
                                 </div>
                             </div>
                             <!-- // Group END -->
@@ -186,21 +186,21 @@
                             <div class="form-group">
                                 <label class="col-md-3 control-label">Area</label>
                                 <div class="col-md-9">
-                                    <input type="text" value="{{'area'}}" class="form-control" />
+                                    <input type="text" value="{{$user->profile->leave != null ? $user->profile->leave->address->house->area->name : ''}}" class="form-control" />
                                 </div>
                             </div>
 
                             <div class="form-group">
                                 <label class="col-md-3 control-label">House No</label>
                                 <div class="col-md-9">
-                                    <input type="text" value="{{'house no'}}" class="form-control" />
+                                    <input type="text" value="{{$user->profile->leave != null ? $user->profile->leave->address->house->house_no : ''}}" class="form-control" />
                                 </div>
                             </div>
 
                             <div class="form-group">
                                 <label class="col-md-3 control-label">House Description</label>
                                 <div class="col-md-9">
-                                    <input type="text" value="{{'house description'}}" class="form-control" />
+                                    <input type="text" value="{{$user->profile->leave != null ? $user->profile->leave->address->house->house_desc : ''}}" class="form-control" />
                                 </div>
                             </div>
                             <!-- // Group END -->
